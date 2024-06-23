54e09d270a9dcd0e4f91a5a74be1dc27024d3540
jo94kr <wls94kr@gmail.com>
2024-06-23 23:28:38 +0900

    revert

--- src/main/java/io/hhplus/clean_architecture/domain/lecture/repository/LectureJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package io.hhplus.clean_architecture.domain.lecture.repository;
-
-import io.hhplus.clean_architecture.domain.lecture.entity.Lecture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LectureJpaRepository extends JpaRepository<Lecture, Long> {
-
-
-}
