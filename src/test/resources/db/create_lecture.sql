0c1b22f4ac1d122d5fdfb441a5394e6a744323c3
JinWoo_ <wls94kr@forbiz.co.kr>
2024-06-25 12:24:06 +0900

    test: test 설정 추가

--- src/test/resources/db/create_lecture.sql
+++ src/test/resources/db/create_lecture.sql
@@ -1,2 +1,2 @@
-insert into lecture(lecture_name, lecture_date, register_cnt, capacity, create_datetime)
-values ('항해 플러스 백엔드', '2024-06-25', 30, 0, '2024-06-25T12:00:00');
+insert into lecture(lecture_name, lecture_datetime, register_cnt, capacity, create_datetime)
+values ('항해 플러스 백엔드', '2024-06-25T13:00:00', 30, 0, '2024-06-25T12:00:00');
